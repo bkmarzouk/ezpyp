07588b26f63c6b2a493202b1557a59532593f0e9
KareemMarzouk <bkmarzouk@outlook.com>
2023-10-31 09:18:37 +0000

    simple crash test + simple dependencies test

--- tests/units/test_pipelines.py
+++ tests/units/test_pipelines.py
@@ -56,3 +56,49 @@ def test_pipeline_with_no_dependencies(tmp_path):
     for step, expected_result in zip((step_foo, step_bar), (2, 4)):
         assert step.get_status() == 0
         assert step.get_result() == expected_result
+
+
+def test_pipeline_that_fails_with_no_dependencies(tmp_path):
+    pipeline = SerialPipeline(tmp_path, "simple")
+
+    @as_pickle_step(pipeline=pipeline, depends_on=[])
+    def foo(crash=True):
+        if crash:
+            raise RuntimeError("Crash")
+        else:
+            return 0
+
+    crash_step = foo()
+
+    pipeline.execute()
+
+    assert crash_step.get_status() == 1
+
+
+def test_pipeline_with_dependencies(tmp_path):
+    pipeline = SerialPipeline(tmp_path, "with_deps")
+
+    @as_pickle_step(pipeline=pipeline, depends_on=[])
+    def foo():
+        return 2
+
+    step_foo = foo()
+
+    @as_pickle_step(pipeline=pipeline, depends_on=[step_foo])
+    def bar():
+        return 123
+
+    step_bar = bar()
+
+    pipeline.organize_steps()
+
+    assert len(pipeline.phases) == 2
+
+    for step in pipeline.steps:
+        assert step.get_status() == -1
+
+    pipeline.execute()
+
+    for step, expected_result in zip((step_foo, step_bar), (2, 123)):
+        assert step.get_status() == 0
+        assert step.get_result() == expected_result
