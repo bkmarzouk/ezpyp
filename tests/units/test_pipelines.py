e349c6fcdddf8a298ea95429646a0f04025ba32e
KareemMarzouk <bkmarzouk@outlook.com>
2023-10-31 08:57:49 +0000

    test simple pipeline with no deps

--- tests/units/test_pipelines.py
+++ tests/units/test_pipelines.py
@@ -27,3 +27,32 @@ def test_attachment(tmp_path):
     # Initialization basically attempts to re-add duplicated step
     with pytest.raises(RepeatedStepError):
         bar()
+
+
+def test_pipeline_with_no_dependencies(tmp_path):
+    pipeline = SerialPipeline(tmp_path, "simple")
+
+    @as_pickle_step(pipeline=pipeline, depends_on=[])
+    def foo():
+        return 2
+
+    step_foo = foo()
+
+    @as_pickle_step(pipeline=pipeline, depends_on=[])
+    def bar(x):
+        return x**2
+
+    step_bar = bar(2)
+
+    pipeline.organize_steps()
+
+    assert len(pipeline.phases) == 1
+
+    for step in pipeline.steps:
+        assert step.get_status() == -1  # Not yet computed !
+
+    pipeline.execute()
+
+    for step, expected_result in zip((step_foo, step_bar), (2, 4)):
+        assert step.get_status() == 0
+        assert step.get_result() == expected_result
